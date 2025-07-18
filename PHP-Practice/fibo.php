9d396d41f59ac7479852153c15af27285ccdb910
mdskaluba <mdskaluba@gmail.com>
2025-07-17 22:20:29 -0400

    Update fibo.php

--- PHP-Practice/fibo.php
+++ PHP-Practice/fibo.php
@@ -1,5 +1,6 @@
 <?php
 $n = isset($_GET['n']) ? intval($_GET['n']) : 0;
+
 $fib = array();
 
 if ($n >= 1) {
@@ -8,6 +9,7 @@ if ($n >= 1) {
 if ($n >= 2) {
     $fib[] = 1;
 }
+
 for ($i = 2; $i < $n; $i++) {
     $fib[] = $fib[$i - 1] + $fib[$i - 2];
 }
